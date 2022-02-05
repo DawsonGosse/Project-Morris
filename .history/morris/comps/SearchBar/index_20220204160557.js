4dc023d2f70949f093f72023c8e1fb69ee02ed0c
KellyMenzul <kmenzul@my.bcit.ca>
2022-02-04 16:07:43 -0800

    remove index material made empty comp

---
+++ .history/morris/comps/SearchBar/index_20220204160557.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+import * as React from 'react';
+import ReactDOM from 'react-dom';
+import Button from '@mui/material/Button';
+import router, {useRouter} from 'next/router';
+
+const Container = styled.div``
+
+// const Buttons = styled.button``
+
+const Buttons = ({
+
+}) => {
+    return <Container>
+        <Button onClick={()=>router.push(routeTo)} style={{width:248, height:75, borderRadius:60,}} variant={variant} type={buttonType}>{ButtonText}</Button>
+    </Container>
+}
+
+export default Buttons;
---
+++ morris/comps/SearchBar/index.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+import * as React from 'react';
+import ReactDOM from 'react-dom';
+import Button from '@mui/material/Button';
+import router, {useRouter} from 'next/router';
+
+const Container = styled.div``
+
+// const Buttons = styled.button``
+
+const SearchBar = ({
+}) => {
+    return <Container>
+    </Container>
+}
+
+export default SearchBar;
